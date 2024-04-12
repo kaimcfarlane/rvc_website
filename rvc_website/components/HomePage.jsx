d5a18cfd43d857c431696927cdbfaa6b34ecaa83
kaimcfarlane <kaimcfarlane43@gmail.com>
2024-04-11 21:07:32 -0400

    Adjust button sizing

--- rvc_website/components/HomePage.jsx
+++ rvc_website/components/HomePage.jsx
@@ -8,15 +8,15 @@ import { useRef } from 'react'
 export default function HomePage() {
   return (
     <section class="bg-[url('../public/rvc_office/rvc_officefloor.jpeg')] bg-cover bg-center h-[100vh] w-[100%] fixed">
-      <div class='bg-opaque_white w-[490px] h-[120px] static ml-[60%] mt-[35%] rounded-md shadow-md'>
-        <div class='flex text-black justify-center font-<K2D> text-3xl pt-[10px] pb-[27px]'>
+      <div class='bg-opaque_white w-[530px] h-[140px] static ml-[60%] mt-[35%] rounded-md shadow-md'>
+        <div class='flex text-black justify-center font-<K2D> text-3xl pt-[20px] pb-[27px]'>
           <div class='mr-[2%]'>COME</div>
           <div class='text-primary mr-[2%]'>SEE</div>
           <div class='mr-[2%]'>THE</div>
           <div class='mr-[2%]'>DIFFERENCE</div>
         </div>
         <div class='flex justify-center mt-[-15px] '>
-          <button className="btn btn-wide btn-primary text-white font-<K2D> text-lg">Request Appointment</button>
+          <button className="btn btn-wide btn-primary text-white font-<K2D> text-lg normal-case font-normal">Request Appointment</button>
         </div>
         <div></div>
       </div>
