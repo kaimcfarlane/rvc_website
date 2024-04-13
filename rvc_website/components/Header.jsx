fe9dd56de8c772b5f2ef6873782cdf4ad16b9b1a
kaimcfarlane <kaimcfarlane43@gmail.com>
2024-04-13 14:55:41 -0400

    Added base code for changing routed text at top of header to be blue when on the page

--- rvc_website/components/Header.jsx
+++ rvc_website/components/Header.jsx
@@ -1,17 +1,46 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import RVC_LOGO from './RVC_LOGO'
 import Link from 'next/link'
 
-export default function header() {
+export default function header({route, changeRoute}) {
+
+  const homeBtn = useRef();
+  const aboutusBtn = useRef();
+  const servicesBtn = useRef();
+  const insuranceBtn = useRef();
+  const contactBtn = useRef();
+
+  useEffect(() => {
+    highlightCurLink();
+  }, [])
+
+  const highlightCurLink = () => {
+    if (route.route == "home") {
+      // Apply UI Changes
+    }
+    else if (route.route == "aboutus") {
+
+    }
+    else if (route.route == "services") {
+      
+    }
+    else if (route.route == "insurance") {
+      
+    }
+    else if (route.route == "contact") {
+      
+    }
+  }
+
   return (
     <section class="flex flex-row justify-between bg-white fixed w-full z-[1]">
         <RVC_LOGO></RVC_LOGO>
         <div id='page_links' class="flex items-center w-6/12 justify-around text-black">
-            <button className="btn btn-ghost"><Link href="/">HOME</Link></button>
-            <button className="btn btn-ghost"><Link href="/aboutus">ABOUT US</Link></button>
-            <button className="btn btn-ghost"><Link href="/services">SERVICES</Link></button>
-            <button className="btn btn-ghost"><Link href="/insurance">INSURANCE</Link></button>
-            <button className="btn btn-ghost"><Link href="/contact">CONTACT</Link></button>
+            <button ref={homeBtn} id='homeBtn' onClick={changeRoute("home")} className="btn btn-ghost"><Link href="/">HOME</Link></button>
+            <button ref={aboutusBtn} id='aboutusBtn' onClick={changeRoute("aboutus")} className="btn btn-ghost"><Link href="/aboutus">ABOUT US</Link></button>
+            <button ref={servicesBtn} id='servicesBtn' onClick={changeRoute("services")} className="btn btn-ghost"><Link href="/services">SERVICES</Link></button>
+            <button ref={insuranceBtn} id='insuranceBtn' onClick={changeRoute("insurance")} className="btn btn-ghost"><Link href="/insurance">INSURANCE</Link></button>
+            <button ref={contactBtn} id='contactBtn' onClick={changeRoute("contact")} className="btn btn-ghost"><Link href="/contact">CONTACT</Link></button>
         </div>
     </section>
   )
--- rvc_website/pages/index.js
+++ rvc_website/pages/index.js
@@ -1,11 +1,18 @@
 import Header from "@/components/Header"
 import HomePage from "@/components/HomePage"
+import { useState } from "react"
 
 
 export default function Home() {
+
+  const [route, setRoute] = useState({route: "home"})
+  const changeRoute = (route) => {
+    setRoute(route);
+  }
+
   return (
     <main>
-      <Header/>
+      <Header route={route} changeRoute={changeRoute}/>
       <HomePage/>
     </main>
   )
