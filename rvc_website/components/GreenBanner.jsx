9736dde4463f3cbad87632cb427e3df10a7181fb
kaimcfarlane <kaimcfarlane43@gmail.com>
2024-04-13 13:48:50 -0400

    Added green banner

--- rvc_website/components/GreenBanner.jsx
+++ rvc_website/components/GreenBanner.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function GreenBanner() {
   return (
-    <div>GreenBanner</div>
+    <div class=" h-[80px] bg-secondary ml-[45%] mt-[3%] rounded-l-full">GreenBanner</div>
   )
 }
