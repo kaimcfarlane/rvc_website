0874b147b4c49cd6af8601953ff897cb0d7a47e8
kaimcfarlane <kaimcfarlane43@gmail.com>
2024-04-13 14:18:04 -0400

    Added different front-end UI frameworks

--- rvc_website/components/Header.jsx
+++ rvc_website/components/Header.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import RVC_LOGO from './RVC_LOGO'
+import Link from 'next/link'
 
 export default function header() {
   return (
     <section class="flex flex-row justify-between bg-white fixed w-full z-[1]">
         <RVC_LOGO></RVC_LOGO>
         <div id='page_links' class="flex items-center w-6/12 justify-around text-black">
-            <button className="btn btn-ghost">HOME</button>
-            <button className="btn btn-ghost">ABOUT US</button>
-            <button className="btn btn-ghost">SERVICES</button>
-            <button className="btn btn-ghost">INSURANCE</button>
-            <button className="btn btn-ghost">CONTACT</button>
+            <button className="btn btn-ghost"><Link href="/">HOME</Link></button>
+            <button className="btn btn-ghost"><Link href="/aboutus">ABOUT US</Link></button>
+            <button className="btn btn-ghost"><Link href="/services">SERVICES</Link></button>
+            <button className="btn btn-ghost"><Link href="/insurance">INSURANCE</Link></button>
+            <button className="btn btn-ghost"><Link href="/contact">CONTACT</Link></button>
         </div>
     </section>
   )
