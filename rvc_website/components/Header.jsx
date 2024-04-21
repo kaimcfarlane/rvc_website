3a5f4194644be31113fe6b7da79583e540313114
kaimcfarlane <kaimcfarlane43@gmail.com>
2024-04-21 13:05:25 -0400

    Added link to entire button

--- rvc_website/components/Header.jsx
+++ rvc_website/components/Header.jsx
@@ -58,11 +58,21 @@ export default function header({route, changeRoute}) {
     <section class="flex flex-row justify-between bg-white fixed w-full z-[1]">
         <RVC_LOGO></RVC_LOGO>
         <div id='page_links' class="flex items-center w-6/12 justify-around text-black">
-            <button ref={homeBtn} id='homeBtn' onClick={() => changeRoute("home")} className="btn btn-ghost"><Link href="/">HOME</Link></button>
-            <button ref={aboutusBtn} id='aboutusBtn' onClick={() => changeRoute("aboutus")} className="btn btn-ghost"><Link href="/aboutus">ABOUT US</Link></button>
-            <button ref={servicesBtn} id='servicesBtn' onClick={() => changeRoute("services")} className="btn btn-ghost"><Link href="/services">SERVICES</Link></button>
-            <button ref={insuranceBtn} id='insuranceBtn' onClick={() => changeRoute("insurance")} className="btn btn-ghost"><Link href="/insurance">INSURANCE</Link></button>
-            <button ref={contactBtn} id='contactBtn' onClick={() => changeRoute("contact")} className="btn btn-ghost"><Link href="/contact">CONTACT</Link></button>
+            <Link href="/">
+              <button ref={homeBtn} id='homeBtn' onClick={() => changeRoute("home")} className="btn btn-ghost">HOME</button>
+            </Link>
+            <Link href="/aboutus">
+              <button ref={aboutusBtn} id='aboutusBtn' onClick={() => changeRoute("aboutus")} className="btn btn-ghost">ABOUT US</button>
+            </Link>
+            <Link href="/services">
+              <button ref={servicesBtn} id='servicesBtn' onClick={() => changeRoute("services")} className="btn btn-ghost">SERVICES</button>
+            </Link>
+            <Link href="/insurance">
+              <button ref={insuranceBtn} id='insuranceBtn' onClick={() => changeRoute("insurance")} className="btn btn-ghost">INSURANCE</button>
+            </Link>
+            <Link href="/contact">
+              <button ref={contactBtn} id='contactBtn' onClick={() => changeRoute("contact")} className="btn btn-ghost">CONTACT</button>
+            </Link>
         </div>
     </section>
   )
