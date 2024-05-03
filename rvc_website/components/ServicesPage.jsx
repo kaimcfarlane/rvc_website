ac28ad2cf5c69751440d22ac21499018e5597bf1
kaimcfarlane <kaimcfarlane43@gmail.com>
2024-05-03 11:10:42 -0400

    Added rest of text for the services page exculind the second green banner from the top

--- rvc_website/components/ServicesPage.jsx
+++ rvc_website/components/ServicesPage.jsx
@@ -69,10 +69,10 @@ export default function ServicesPage() {
             <div class='w-[30%] h-[50%] p-[20px] static ml-[10%] mt-[10%] rounded-md justify-center'>
                 <div class='flex flex-col items-center text-black justify-center font-<K2D> text-5xl pb-[25px]'>
                     <div class="flex flex-row pb-[10px] w-[100%] justify-left">
-                    <div class="mr-[2%] text-secondary text-4xl">Pediatric Exams</div>
+                    <div class="mr-[2%] text-secondary text-4xl">Contact Lenses</div>
                     </div>
                     <div class="flex flex-col w-[100%] justify-left mb-[10px]">
-                        <div class="text-lg mb-[10px]">Approximately 25% of school-aged children have vision problems. All children should have a comprehensive eye exam before age 3.  It is very important for parents to know that vision screenings performed at school or by a pediatrician are not substitutes for a comprehensive eye exam.  At Reese Vision Care, we see children age 3 years and older.</div>
+                        <div class="text-lg mb-[10px]">Dr. Reese is a contact lens specialist.  Dr. Reese fits contact lenses of all types, including color lenses, lenses for astigmatism, and multi-focal lenses. Whether you prefer daily, weekly or monthly disposables, we provide a wide selection of contact lenses to fit your needs. </div>
                     </div>
                 </div>
             </div>
@@ -86,14 +86,14 @@ export default function ServicesPage() {
             <div class='w-[30%] h-[50%] p-[20px] static ml-[63%] mt-[10%] rounded-md justify-center'>
                 <div class='flex flex-col items-center text-black justify-center font-<K2D> text-5xl pb-[25px]'>
                     <div class="flex flex-row pb-[10px] w-[100%] justify-left">
-                    <div class="mr-[2%] text-secondary text-4xl">Pediatric Exams</div>
+                    <div class="mr-[2%] text-secondary text-4xl">Eyewear</div>
                     </div>
                     <div class="flex flex-col w-[100%] justify-left mb-[10px]">
-                        <div class="text-lg mb-[10px]">Approximately 25% of school-aged children have vision problems. All children should have a comprehensive eye exam before age 3.  It is very important for parents to know that vision screenings performed at school or by a pediatrician are not substitutes for a comprehensive eye exam.  At Reese Vision Care, we see children age 3 years and older.</div>
+                        <div class="text-lg mb-[10px]">A pair of glasses isn’t just about having great sight, it’s about looking great too. We offer a complete optical showroom with quality eyewear for every lifestyle and budget.  Our knowledgeable and friendly staff will help you choose the perfect eyewear for you!</div>
                     </div>
                 </div>
             </div>
-            <GreenRectangleBanner text={"We have lots of frames that look cool and fit well so kids actually love their glasses! We use durable products and safe polycarbonate lenses.  We also offer contact lenses for children.  Disposable contact lenses are safer and more convenient making them a great option for kids, especially for sports and other activities."}></GreenRectangleBanner>
+            <GreenRectangleBanner text={"Designer Frames - Prescription Eyeglasses - Safety Glasses - Sports Glasses - Blue Light Glasses - Sunglasses"}></GreenRectangleBanner>
         </section>
     </>
   )
