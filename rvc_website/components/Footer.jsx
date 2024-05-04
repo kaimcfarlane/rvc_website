a9efaee28d9a292ddc23018d66dda2ced0ace979
kaimcfarlane <kaimcfarlane43@gmail.com>
2024-05-04 11:27:18 -0400

    Added phone number to footer

--- rvc_website/components/Footer.jsx
+++ rvc_website/components/Footer.jsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import RVC_ICON from './RVC_ICON'
 import email_icon from '../public/gen_icons/email_icon_4.svg'
+import phone_icon from '../public/gen_icons/phone_icon_1.svg'
 
 export default function Footer({changeRoute, background, pages}) {
 
@@ -126,8 +127,8 @@ export default function Footer({changeRoute, background, pages}) {
             <Link href="https://www.facebook.com/reesevisioncare/" target="_blank">
               <div><Image src={email_icon} width="24" height="24" viewBox="0 0 24 24" className="fill-current" class="fill-white"></Image></div>
             </Link>
-            <Link href="https://www.facebook.com/reesevisioncare/" target="_blank">
-              <div><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></div>
+            <Link href="tel:9547464009" target="_blank">
+              <div><Image src={phone_icon} width="24" height="24" viewBox="0 0 24 24" className="fill-current"></Image></div>
             </Link>
             </div>
         </nav>
