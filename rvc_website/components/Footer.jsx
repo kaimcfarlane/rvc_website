6cd74acd6f197b9cc20ee542c94eaea5165405c6
kaimcfarlane <kaimcfarlane43@gmail.com>
2024-05-04 11:21:17 -0400

    Added footer info

--- rvc_website/components/Footer.jsx
+++ rvc_website/components/Footer.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import RVC_ICON from './RVC_ICON'
+import email_icon from '../public/gen_icons/email_icon_4.svg'
 
 export default function Footer({changeRoute, background, pages}) {
 
@@ -49,11 +50,19 @@ export default function Footer({changeRoute, background, pages}) {
     <div class="w-[100%] absolute" style={{top: footerPosition}}>
         <footer ref={footerTopContainer} className="footer p-10 bg-white text-base-content">
         <nav>
-            <h6 ref={footerTitle} className={`footer-title ${footerTitleClass}`}>Services</h6> 
-            <a className="link link-hover">Branding</a>
-            <a className="link link-hover">Design</a>
-            <a className="link link-hover">Marketing</a>
-            <a className="link link-hover">Advertisement</a>
+            <h6 ref={footerTitle} className={`footer-title ${footerTitleClass}`}>Information</h6> 
+            <Link href="https://www.facebook.com/reesevisioncare/" target="_blank">
+              <div className="link link-hover">Branding</div>
+            </Link>
+            <Link href="https://www.facebook.com/reesevisioncare/reviews" target="_blank">
+              <div className="link link-hover">Reviews</div>
+            </Link>
+            <Link href="https://www.facebook.com/reesevisioncare/photos" target="_blank">
+              <div className="link link-hover">Photos</div>
+            </Link>
+            <Link href="insurance">
+              <div onClick={() => changeRoute("insurance")} className="link link-hover">Insurance</div>
+            </Link>
         </nav> 
         <nav>
             <h6 ref={footerTitle} className={`footer-title ${footerTitleClass}`}>Company</h6> 
@@ -63,18 +72,45 @@ export default function Footer({changeRoute, background, pages}) {
             <Link href="/contact">
               <div onClick={() => changeRoute("contact")} className="link link-hover">Contact</div>
             </Link>
-            <Link href="/">
+            <Link href="/" target="_blank">
               <div className="link link-hover">Jobs</div>
             </Link>
-            <Link href="/">
-              <div className="link link-hover">Press kit</div>
+            <Link href="http://maps.google.com/?q=3801 N University Drive, Suite 301 Sunrise, FL 33351" target="_blank">
+              <div className="link link-hover">Address</div>
             </Link>
         </nav> 
         <nav>
             <h6 ref={footerTitle} className={`footer-title ${footerTitleClass}`}>Legal</h6> 
-            <a className="link link-hover">Terms of use</a>
-            <a className="link link-hover">Privacy policy</a>
-            <a className="link link-hover">Cookie policy</a>
+            <div onClick={()=>document.getElementById('TOU_modal').showModal()} className="link link-hover">Terms of use</div>
+            <dialog id="TOU_modal" className="modal">
+              <div className="modal-box">
+                <h3 className="font-bold text-lg text-black">Hello there!</h3>
+                <p className="py-4 text-black">LEGAL DOCUMENT HERE</p>
+              </div>
+              <form method="dialog" className="modal-backdrop">
+                <button>close</button>
+              </form>
+            </dialog>
+            <div onClick={()=>document.getElementById('privacy_policy_modal').showModal()} className="link link-hover">Privacy policy</div>
+            <dialog id="privacy_policy_modal" className="modal">
+              <div className="modal-box">
+                <h3 className="font-bold text-lg text-black">Hello there!</h3>
+                <p className="py-4 text-black">PRIVACY POLICY HERE</p>
+              </div>
+              <form method="dialog" className="modal-backdrop">
+                <button>close</button>
+              </form>
+            </dialog>
+            <div onClick={()=>document.getElementById('cookies_modal').showModal()} className="link link-hover">Cookie policy</div>
+            <dialog id="cookies_modal" className="modal">
+              <div className="modal-box">
+                <h3 className="font-bold text-lg text-black">Hello there!</h3>
+                <p className="py-4 text-black">This website uses no cookies</p>
+              </div>
+              <form method="dialog" className="modal-backdrop">
+                <button>close</button>
+              </form>
+            </dialog>
         </nav>
         </footer> 
         <footer ref={footerBotContainer} className="footer px-10 py-4 border-t bg-white text-base-content border-base-300">
@@ -84,9 +120,15 @@ export default function Footer({changeRoute, background, pages}) {
         </aside> 
         <nav className="md:place-self-center md:justify-self-end">
             <div className="grid grid-flow-col gap-4">
-            <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a>
-            <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a>
-            <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
+            <Link href="https://www.facebook.com/reesevisioncare/" target="_blank">
+              <div><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></div>
+            </Link>
+            <Link href="https://www.facebook.com/reesevisioncare/" target="_blank">
+              <div><Image src={email_icon} width="24" height="24" viewBox="0 0 24 24" className="fill-current" class="fill-white"></Image></div>
+            </Link>
+            <Link href="https://www.facebook.com/reesevisioncare/" target="_blank">
+              <div><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></div>
+            </Link>
             </div>
         </nav>
         </footer>
