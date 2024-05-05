b8a1f7cd9717a582b612ddbaa85087f18993fcb0
kaimcfarlane <kaimcfarlane43@gmail.com>
2024-05-04 20:01:22 -0400

    Added icons to footer social bar

--- rvc_website/components/Footer.jsx
+++ rvc_website/components/Footer.jsx
@@ -3,8 +3,8 @@ import Image from 'next/image'
 import Link from 'next/link'
 import ContactModal from './ContactModal'
 import RVC_ICON from './RVC_ICON'
-import email_icon from '../public/gen_icons/email_icon_4.svg'
-import phone_icon from '../public/gen_icons/phone_icon_1.svg'
+import email_icon from '../public/gen_icons/email_icon_5.png'
+import phone_icon from '../public/gen_icons/phone_icon_3.png'
 
 export default function Footer({changeRoute, background, pages}) {
 
@@ -120,12 +120,12 @@ export default function Footer({changeRoute, background, pages}) {
             <p>Reese Vision Care Opt. <br/>Providing quality eye care since 2004</p>
         </aside> 
         <nav className="md:place-self-center md:justify-self-end">
-            <div className="grid grid-flow-col gap-4">
+            <div className="grid grid-flow-col gap-4 items-center">
             <Link href="https://www.facebook.com/reesevisioncare/" target="_blank">
               <div><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></div>
             </Link>
             <Link href="mailto:reesevisioncontacts@gmail.com" target="_blank">
-              <div><Image src={email_icon} width="24" height="24" viewBox="0 0 24 24" className="fill-current" class="fill-white"></Image></div>
+              <div><Image src={email_icon} width="33" height="33" viewBox="0 0 24 24" className="fill-current"></Image></div>
             </Link>
             <Link href="tel:9547464009" target="_blank">
               <div><Image src={phone_icon} width="24" height="24" viewBox="0 0 24 24" className="fill-current"></Image></div>
