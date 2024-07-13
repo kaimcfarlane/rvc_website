5cf8ed26e40b0983f0f49a0ebab25fe22b17f827
kaimcfarlane <kaimcfarlane43@gmail.com>
2024-07-13 19:09:16 -0400

    Added larger screen styling to about page

--- rvc_website/components/AboutPage.jsx
+++ rvc_website/components/AboutPage.jsx
@@ -11,12 +11,12 @@ export default function AboutPage() {
     <>
     <section class="h-[100vh] w-[100%] absolute">
       <div>
-        <Image src={CircleBackground} class="w-[500px] h-[500px] z-0 absolute top-[25.5%] left-[10%]" alt='CircleBackground'></Image>
-        <Image src={DocPatient_pic1} class="w-[500px] h-[500px] z-1 absolute top-[25%] left-[10.5%]" alt='DocPatientPic1'></Image>
+        <Image src={CircleBackground} class="w-[33vw] h-[33vw] z-0 absolute top-[25.5%] left-[10%]" alt='CircleBackground'></Image>
+        <Image src={DocPatient_pic1} class="w-[33vw] h-[33vw] z-1 absolute top-[25%] left-[10.5%]" alt='DocPatientPic1'></Image>
       </div>
       <div class='w-[45%] h-[50%] p-[20px] static ml-[50%] mt-[15%] rounded-md justify-center'>
-        <div class='flex flex-col items-center text-black justify-center font-<K2D> text-5xl pb-[25px]'>
-            <div class="flex flex-row pb-[20px] w-[100%] justify-left">
+        <div class='flex flex-col items-center text-black justify-center font-<K2D> text-[3.18vw] pb-[25px]'>
+            <div class="flex flex-row pb-[1.3vw] w-[100%] justify-left">
                 <div class="mr-[2%]">Our</div>
                 <div class="text-primary">Optometry,</div>
             </div>
@@ -25,7 +25,7 @@ export default function AboutPage() {
                 <div class="text-secondary">Eye Care</div>
             </div>
         </div>
-        <div class='flex text-black justify-center font-<K2D> text-3xl pt-[20px] pb-[27px]'>
+        <div class='flex text-black justify-center font-<K2D> text-[1.99vw] leading-[2.38vw] pt-[20px] pb-[27px]'>
           <div class="">
             Welcome to Reese Vision Care, where we are dedicated to providing exceptional (eye care) vision services to satisfy all your eye care needs. <mark class="text-primary bg-transparent">Dr. Reese</mark> and her staff are highly knowledgeable and devoted to excellent patient care. We offer quality eye care in a comfortable, family-friendly environment.
           </div>
