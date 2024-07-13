b3404a00bec673a914b03a27ba8a975f267a6757
kaimcfarlane <kaimcfarlane43@gmail.com>
2024-07-13 17:57:03 -0400

    Fixed home page for larger screen sizes

--- rvc_website/components/HomePage.jsx
+++ rvc_website/components/HomePage.jsx
@@ -13,15 +13,15 @@ export default function HomePage() {
 
   return (
     <section class="bg-[url('../public/rvc_office/rvc_officefloor.jpeg')] bg-cover bg-center h-[100vh] w-[100%] absolute">
-      <div class='bg-opaque_white w-[530px] h-[140px] static ml-[60%] mt-[32%] rounded-md shadow-md'>
-        <div class='flex text-black justify-center font-<K2D> text-3xl pt-[20px] pb-[27px]'>
+      <div class='bg-opaque_white w-[35vw] h-[9.3vw] static ml-[60%] mt-[32%] rounded-md shadow-md'>
+        <div class='flex text-black justify-center font-<K2D> text-[1.99vw] pt-[1.31vw] pb-[1.8vw]'>
           <div class='mr-[2%]'>COME</div>
           <div class='text-primary mr-[2%]'>SEE</div>
           <div class='mr-[2%]'>THE</div>
           <div class='mr-[2%]'>DIFFERENCE</div>
         </div>
         <div class='flex justify-center mt-[-15px] '>
-          <button onClick={()=>document.getElementById('my_modal_1').showModal()} className="btn btn-wide btn-primary text-white font-<K2D> text-lg normal-case font-normal">Request Appointment</button>
+          <button onClick={()=>document.getElementById('my_modal_1').showModal()} className="btn w-[17vw] h-[2.5vw] btn-primary text-white font-<K2D> text-[1vw] normal-case font-normal">Request Appointment</button>
           <dialog id="my_modal_1" className="modal"><ContactModal text={"Fill out form to request an appointment"} placeholder={"Appointment Request"} isJobForm={false}></ContactModal></dialog>
         </div>
       </div>
