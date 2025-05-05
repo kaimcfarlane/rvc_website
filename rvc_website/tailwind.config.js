d17b4db016f139f208416f8503d2a2a3cca35bcc
kaimcfarlane <kaimcfarlane43@gmail.com>
2025-05-04 22:49:58 -0400

    Updated tailwind config

--- rvc_website/tailwind.config.js
+++ rvc_website/tailwind.config.js
@@ -7,31 +7,22 @@ module.exports = {
   ],
   theme: {
     extend: {
-      mytheme: {
-        "primary": "#0094DA",
-        "secondary": "#22c55e",
-        "accent": "#FFFFFF",
-        "neutral": "#000000",
-        "base-100": "#000000"
+      fontFamily: {
+        'nunito': ['nunito', 'sans-serif'],
+        'K2D': ['K2D', 'sans-serif']
       },
-    },
-    fontFamily: {
-      'nunito': ['nunito', 'sans-serif'],
-      'K2D': ['K2D', 'sans-serif'] // Ensure fonts with spaces have " " surrounding it.
-    },
-    colors: {
-      primary: '#0094DA', // Blue
-      primary_daisy: '#377cfb', // Green
-      secondary: '#22c55e', // Green
-      secondary_daisy: '#66cc8a', // Blue
-      exact_secondary: '#85B446',
-      white: '#FFFFFF',
-      opaque_white: "rgb(255 255 255 / 90%)",
-      black: '#000000',
-      subtext: '#666666',     // custom light gray
-      darktext: '#1a1a1a',     // near-black readable text
-    },
-    extend: {
+      colors: {
+        primary: '#0094DA',
+        primary_daisy: '#377cfb',
+        secondary: '#22c55e',
+        secondary_daisy: '#66cc8a',
+        exact_secondary: '#85B446',
+        white: '#FFFFFF',
+        opaque_white: "rgb(255 255 255 / 90%)",
+        black: '#000000',
+        subtext: '#666666',
+        darktext: '#1a1a1a',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
@@ -43,7 +34,7 @@ module.exports = {
   daisyui: {
     themes: [
       {
-      'mytheme': {
+        'mytheme': {
           'primary': '#0094DA',
           'secondary': '#22c55e',
           'accent': '#FFFFFF',
